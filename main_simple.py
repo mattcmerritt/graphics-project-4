0b1290676f33c1b0028621d5830191c8c6bc5835
mattcmerritt <mattcmerritt@gmail.com>
2024-12-14 01:54:03 +0000

    rebuilt room

--- main_simple.py
+++ main_simple.py
@@ -67,7 +67,7 @@ raytrace_count = 0  # How many ray traced images have been generated so far
 block_size = 4
 
 # Functions
-def set_light_positions(lightA):
+def set_looping_light_positions(lightA):
     global light_angle, light_distance
     pos_x = light_distance * math.cos(math.radians(light_angle))
     pos_y = light_distance * math.sin(math.radians(light_angle))
@@ -86,31 +86,56 @@ def init_scene():
     cam.look_at(init_eye, init_look, init_up)
     cam.set_lens_shape(init_view_angle, FRAME_WIDTH / FRAME_HEIGHT, init_near, init_far)
 
-    # Create objects and add them to the scene
-    # mat = Material()
-    # mat.set_copper()
-    # mat.set_reflectivity(0.1)
-    # ball = SphereObj()
-    # ball.set_material(mat)
-    # ball.translate(0,1,0)
-    # ball.scale(1, 2, 1)
-    # ball.name = "Ball 1"
-    # scn.add_object(ball)
-
-    # TODO: move elsewhere
-    mat = Material()
-    mat.set_copper()
-    mat.set_reflectivity(0.1)
-
     # boxes
-    textured_box = BoxObj()
-    textured_box.set_texture('resources/example_texture.png', 128)
-    textured_box.set_normal_map('resources/beveled_edges.png')
-    textured_box.set_material(mat)
-    textured_box.translate(0,1,0)
-    textured_box.scale(1, 2, 1)
-    textured_box.name = "Textured Box 1"
-    scn.add_object(textured_box)
+    mat = Material()
+    mat.set_gold()
+    mat.set_reflectivity(0.4)
+    textured_box_1 = BoxObj()
+    textured_box_1.set_material(mat)
+    textured_box_1.translate(0,1,-10)
+    textured_box_1.scale(0.5, 0.5, 0.5)
+    textured_box_1.rotate(45, Vector3(0, 1, 0))
+    textured_box_1.rotate(45, Vector3(1, 0, 0))
+    textured_box_1.name = "Box 1"
+    scn.add_object(textured_box_1)
+
+    mat = Material()
+    mat.set_chrome()
+    mat.set_reflectivity(0.7)
+    textured_box_2 = BoxObj()
+    textured_box_2.set_texture('resources/example_texture.png', 128)
+    textured_box_2.set_material(mat)
+    textured_box_2.translate(10,1,11)
+    textured_box_2.scale(1, 2, 1)
+    textured_box_2.name = "Box 2"
+    scn.add_object(textured_box_2)
+
+    mat = Material()
+    mat.set_pewter()
+    mat.set_reflectivity(0.3)
+    textured_box_3 = BoxObj()
+    textured_box_3.set_texture('resources/striped.png', 128)
+    textured_box_3.set_normal_map('resources/beveled_edges.png')
+    textured_box_3.set_material(mat)
+    textured_box_3.translate(-7,0.5,-7)
+    textured_box_3.scale(0.75, 0.75, 0.75)
+    textured_box_3.rotate(45, Vector3(0, 1, 0))
+    textured_box_3.rotate(45, Vector3(1, 0, 0))
+    textured_box_3.name = "Box 3"
+    scn.add_object(textured_box_3)
+
+    mat = Material()
+    mat.set_silver()
+    mat.set_reflectivity(0.1)
+    mat.set_shininess(10)
+    textured_box_4 = BoxObj()
+    textured_box_4.set_texture('resources/grid.png', 128)
+    textured_box_4.set_normal_map('resources/grid.png')
+    textured_box_4.set_material(mat)
+    textured_box_4.translate(-6,0,10)
+    textured_box_4.rotate(45, Vector3(0, 1, 0))
+    textured_box_4.name = "Box 4"
+    scn.add_object(textured_box_4)
 
     # tapered cylinders
     mat = Material()
@@ -118,7 +143,7 @@ def init_scene():
     mat.set_reflectivity(0.7)
     tube = CylinderObj(1, 2, 3)
     tube.set_material(mat)
-    tube.translate(5,1,2)
+    tube.translate(8,1,2)
     tube.scale(1, 1, 1)
     tube.name = "Tube 1"
     scn.add_object(tube)
@@ -128,7 +153,7 @@ def init_scene():
     mat.set_reflectivity(0.7)
     tube2 = CylinderObj(0, 1, 3)
     tube2.set_material(mat)
-    tube2.translate(3,1,0)
+    tube2.translate(-9,1,0)
     tube2.rotate(90, Vector3(1, 0, 0))
     tube2.scale(1, 1, 1)
     tube2.name = "Tube 2"
@@ -157,41 +182,41 @@ def init_scene():
 
     # balls
     mat = Material()
-    mat.set_gold()
-    mat.set_reflectivity(0.1)
-    ball = SphereObj()
-    ball.set_material(mat)
-    ball.translate(5,1,0)
-    ball.scale(1, 2, 1)
-    ball.name = "Ball 1"
-    scn.add_object(ball)
+    mat.set_copper()
+    mat.set_reflectivity(0.4)
+    ball1 = SphereObj()
+    ball1.set_material(mat)
+    ball1.translate(-2,0,-10)
+    ball1.scale(1, 1, 1)
+    ball1.name = "Ball 1"
+    scn.add_object(ball1)
 
     mat = Material()
-    mat.set_copper()
-    mat.set_reflectivity(0.8)
+    mat.set_silver()
+    mat.set_reflectivity(0.4)
     ball2 = SphereObj()
     ball2.set_material(mat)
-    ball2.translate(-4,0,-8)
+    ball2.translate(2,0,-10)
     ball2.scale(1, 1, 1)
     ball2.name = "Ball 2"
     scn.add_object(ball2)
 
     mat = Material()
-    mat.set_silver()
-    mat.set_reflectivity(0.8)
+    mat.set_gold()
+    mat.set_reflectivity(0.1)
     ball3 = SphereObj()
     ball3.set_material(mat)
-    ball3.translate(-8,0,-8)
-    ball3.scale(1, 1, 1)
+    ball3.translate(8,1,0)
+    ball3.scale(1, 2, 1)
     ball3.name = "Ball 3"
     scn.add_object(ball3)
 
     mat = Material()
     mat.set_chrome()
-    mat.set_reflectivity(0.4)
+    mat.set_reflectivity(0.7)
     ball4 = SphereObj()
     ball4.set_material(mat)
-    ball4.translate(-10,0,10)
+    ball4.translate(-10,0,3)
     ball4.scale(2, 1, 2)
     ball4.name = "Ball 4"
     scn.add_object(ball4)
@@ -199,7 +224,7 @@ def init_scene():
     # walls
     mat = Material()
     mat.set_silver()
-    mat.set_reflectivity(0.8)
+    mat.set_reflectivity(0.3)
     floor = BoxObj()
     floor.name = "Floor"
     floor.set_material(mat)
@@ -207,11 +232,10 @@ def init_scene():
     floor.scale(15, 0.1, 15)
     scn.add_object(floor)
 
-    # TODO: REPLACE LATER
-    mat = Material()
-    mat.set_pewter()
+    mat = Material() # uses the flat default material
     mat.set_reflectivity(0.1)
     forward_wall = BoxObj()
+    forward_wall.set_texture('resources/lattice.png', 128)
     forward_wall.name = "Forward Wall"
     forward_wall.set_material(mat)
     forward_wall.translate(0, 3, -15)
@@ -219,6 +243,7 @@ def init_scene():
     scn.add_object(forward_wall)
 
     backward_wall = BoxObj()
+    backward_wall.set_texture('resources/lattice.png', 128)
     backward_wall.name = "Backward Wall"
     backward_wall.set_material(mat)
     backward_wall.translate(0, 3, 15)
@@ -226,6 +251,7 @@ def init_scene():
     scn.add_object(backward_wall)
 
     left_wall = BoxObj()
+    left_wall.set_texture('resources/lattice.png', 128)
     left_wall.name = "Left Wall"
     left_wall.set_material(mat)
     left_wall.translate(-15, 3, 0)
@@ -233,6 +259,7 @@ def init_scene():
     scn.add_object(left_wall)
 
     right_wall = BoxObj()
+    right_wall.set_texture('resources/lattice.png', 128)
     right_wall.name = "Right Wall"
     right_wall.set_material(mat)
     right_wall.translate(15, 3, 0)
@@ -249,11 +276,56 @@ def init_scene():
     # Create a visible component of the light
     # It will be associated both with the light and with the Scene
     lightA.obj = SphereObj()
-    lightA.obj.name = "Light Source"
+    lightA.obj.name = "Light Source A"
     lightA.obj.set_material(mat)
     scn.add_object(lightA.obj)
 
-    set_light_positions(lightA)
+    set_looping_light_positions(lightA)
+
+    # Stationary lights
+    lightB = Light()
+    scn.add_light(lightB)
+    mat = Material()
+    mat.set_emissive_only(lightB.get_diffuse())
+    mat.set_translucent(True)
+    lightB.obj = SphereObj()
+    lightB.obj.name = "Light Source B"
+    lightB.obj.set_material(mat)
+    scn.add_object(lightB.obj)
+    lightB.set_position(0, 5, -8)
+    lightB.obj.reset()
+    lightB.obj.translate(0, 5, -8)
+    lightB.obj.scale(0.2, 0.2, 0.2)
+
+    # Stationary lights
+    lightC = Light()
+    scn.add_light(lightC)
+    mat = Material()
+    mat.set_emissive_only(lightC.get_diffuse())
+    mat.set_translucent(True)
+    lightC.obj = SphereObj()
+    lightC.obj.name = "Light Source C"
+    lightC.obj.set_material(mat)
+    scn.add_object(lightC.obj)
+    lightC.set_position(-8, 5, 8)
+    lightC.obj.reset()
+    lightC.obj.translate(-8, 5, 8)
+    lightC.obj.scale(0.2, 0.2, 0.2)
+
+    # Stationary lights
+    lightD = Light()
+    scn.add_light(lightD)
+    mat = Material()
+    mat.set_emissive_only(lightD.get_diffuse())
+    mat.set_translucent(True)
+    lightD.obj = SphereObj()
+    lightD.obj.name = "Light Source D"
+    lightD.obj.set_material(mat)
+    scn.add_object(lightD.obj)
+    lightD.set_position(8, 5, 8)
+    lightD.obj.reset()
+    lightD.obj.translate(8, 5, 8)
+    lightD.obj.scale(0.2, 0.2, 0.2)
 
 # Return a copy of all the things in scene that could have changed!
 def get_copy_state():
@@ -264,7 +336,7 @@ def restore_state(state):
     global nav, light_angle, lightA
     nav.camera = copy.deepcopy(state[0])
     light_angle = state[1]
-    set_light_positions(lightA)
+    set_looping_light_positions(lightA)
 
 def display():
     global render_mode, raytrace_count, animate, record
@@ -369,7 +441,7 @@ def main():
             if light_angle >= 360: light_angle -= 360
             elif light_angle < 0: light_angle += 360
 
-            set_light_positions(lightA)
+            set_looping_light_positions(lightA)
 
         display()
         
